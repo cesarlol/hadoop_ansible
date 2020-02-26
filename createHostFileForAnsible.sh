9adaebd8955abbf27ebf96a914cb13799bb7f22c
Cesar Lopez <eng.cesar.lopez@gmail.com>
2020-02-25 21:34:19 -0600

    update for aws starter

--- createHostFileForAnsible.sh
+++ createHostFileForAnsible.sh
@@ -3,10 +3,10 @@ if [ -z "$1" ];
 fi
 
 echo "[iteso]"  > hosts_ansible
-aws ec2 describe-instances --profile iteso --region=us-west-2 --query 'Reservations[*].Instances[*].[InstanceId,Tags[?Key==`Name`].Value|[0],State.Name,PublicDnsName,PublicIpAddress,PrivateIpAddress]' --filters Name=instance-state-name,Values=running --output table | grep ec2 | awk '{print $11 "  hostname="$3".example.com" }' >> hosts_ansible
+aws ec2 describe-instances --profile iteso --region=us-east-1 --query 'Reservations[*].Instances[*].[InstanceId,Tags[?Key==`Name`].Value|[0],State.Name,PublicDnsName,PublicIpAddress,PrivateIpAddress]' --filters Name=instance-state-name,Values=running --output table | grep ec2 | awk '{print $11 "  hostname="$3".example.com" }' >> hosts_ansible
 
 echo "[iteso:vars]" >> hosts_ansible
 echo "ansible_user=centos" >> hosts_ansible
-echo "ansible_ssh_private_key_file=/home/centos/$1" >> hosts_ansible
+echo "ansible_ssh_private_key_file=/home/ec2-user/$1" >> hosts_ansible
 
 cat hosts_ansible
